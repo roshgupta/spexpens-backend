92f8219662c0bf68b2c5942d8d96ba457dbfaf0b
roshgupta <gpta.duke.rohan.1219@gmail.com>
2024-01-01 22:54:43 +0530

    chore: added helmet and cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors());
+app.use(helmet());
 
 app.use('/', (req, res) => {
   res.json({ key: 'Hello' });
