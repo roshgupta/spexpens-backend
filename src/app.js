10add94d51a8e64277c9f4bde0922bdf7e9e393a
roshgupta <gpta.duke.rohan.1219@gmail.com>
2024-01-02 19:18:22 +0530

    fix: helmet should be placed first in middleware sequence

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import helmet from 'helmet';
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
 app.use(helmet());
+app.use(cors());
 
 app.use('/', (req, res) => {
   res.json({ key: 'Hello' });
