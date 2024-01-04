1bdd81f0f70247c0aac3b6c75deab5ea122313c5
roshgupta <gpta.duke.rohan.1219@gmail.com>
2024-01-04 15:16:16 +0530

    min: added a comment

--- src/models/user.js
+++ src/models/user.js
@@ -37,6 +37,7 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     next();
+    //! TODO: TEST IS USING THIS NEXT, IT Actually returns from here.
   }
 
   const salt = await bcrypt.genSalt(10);
